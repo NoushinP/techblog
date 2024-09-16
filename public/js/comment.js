be851381e92e17541bf0283c22c387fa88846f7d
NoushinP <noushinazari89@gmail.com>
2024-09-16 00:01:59 -0500

    update main.handlebars and comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -19,7 +19,7 @@ const commentFormHandler = async function (event) {
       if (response.ok) {
         document.location.reload();
       } else {
-        document.location.replace('/login');
+        document.location.replace('');
       }
     }
   };
